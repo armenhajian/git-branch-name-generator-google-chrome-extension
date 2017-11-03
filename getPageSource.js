8ddb24d43e3ea571139c7f6af1a52d75a0d5f36e
armenhajian <rmenhajian@gmail.com>
2017-11-03 10:21:30 +0000

    common improvements atlassian added also

--- getPageSource.js
+++ getPageSource.js
@@ -1,13 +1,53 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.sendData)
+  function (request, sender, sendResponse) {
+    if (request.sendData) {
       sendResponse({
-        issue: {
-          title: document.getElementsByClassName('js-issue-title')[0].innerHTML,
-          number: document.getElementsByClassName('gh-header-number')[0].innerHTML,
-          types: Object.keys(document.getElementsByClassName('label')).map(function (key) {
-            return document.getElementsByClassName('label')[key].innerHTML;
-          })
-        }
-      });
-  });
+        issue: getIssue(getService(request.url), document)
+      })
+    }
+
+    function getIssue(service, document) {
+      var issue = {};
+      switch (service) {
+        case 'github':
+          issue = getGithub(document);
+          break;
+        case 'atlassian':
+          issue = getAtlassian(document);
+          break;
+      }
+
+      return issue;
+    }
+
+    function getGithub(document) {
+      return {
+        title: document.getElementsByClassName('js-issue-title')[0].innerHTML,
+        number: document.getElementsByClassName('gh-header-number')[0].innerHTML,
+        types: Object.keys(document.getElementsByClassName('label')).map(function (key) {
+          return document.getElementsByClassName('label')[key].innerHTML;
+        })
+      }
+    }
+
+    function getAtlassian(document) {
+      return {
+        title: document.getElementById('summary-val').innerHTML,
+        number: document.getElementsByClassName('issue-link')[0].innerHTML,
+        types: []//@todo: implement right logic for this
+      }
+    }
+
+    function getService(url) {
+      var service = '';
+
+      if (url.indexOf('github') > 0) {
+        service = 'github';
+      } else if (url.indexOf('atlassian') > 0) {
+        service = 'atlassian'
+      }
+
+      return service;
+    }
+  }
+);
--- popup.js
+++ popup.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   var checkPageButton = document.getElementById('branch-name');
 
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {sendData: true}, function (res) {
+    chrome.tabs.sendMessage(tabs[0].id, {sendData: true, url: tabs[0].url}, function (res) {
       function githubIssueTitleToBranchName() {
-        var title = res.issue.title.trim().replace(new RegExp(' ', 'g'), '-').replace('[','').replace(']','').replace(',','');
+        //@todo: improve Regex logic and combine
+        var title = res.issue.title.trim()
+          .replace(/-/g, ' ')
+          .replace(/_/g, ' ')
+          .replace(/\[/g, '')
+          .replace(/]/g, '')
+          .replace(/,/g, '')
+          .replace(/  +/g, ' ')
+          // this is the last and general replacing for title
+          .replace(new RegExp(' ', 'g'), '-');
         var number = res.issue.number.replace('#', '');
         var type = 'feature';
         res.issue.types.forEach(function (t) {
